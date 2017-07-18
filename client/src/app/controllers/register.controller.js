57f3c19c1129402cf70473728749758bfa53d078
natansevero <natanseverolp@gmail.com>
2017-07-18 01:54:16 -0300

    constructing view register person

--- client/src/app/controllers/register.controller.js
+++ client/src/app/controllers/register.controller.js
@@ -6,7 +6,29 @@
 
   function RegisterController() {
     var vm = this;
+    var location;
 
-    vm.teste = "teste";
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var mapid = L.map('map').setView([position.coords.latitude, position.coords.longitude], 6);
+      tileLayerMap(mapid);
+
+      var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mapid);
+
+      location = setLocation(position.coords.latitude, position.coords.longitude);
+    });
+
+  }
+
+  function tileLayerMap(map) {
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+
+  function setLocation(lat, lng) {
+    return {
+      lat: lat,
+      lng: lng
+    }
   }
 })();
